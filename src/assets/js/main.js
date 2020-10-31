b3b7d5d5e2701cdf2911655ef0b522c1c535c1e9
Evgeniy Shek <shek@chocotravel.com>
2020-10-31 15:03:30 +0600

    feature(main-js): add collapse and burger logic

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,24 +1,54 @@
 /* eslint-disable */
 
 $(document).ready(function(){
+    initBurger();
+    initCityModal();
+    initCollapse();
+});
+
+function initBurger() {
+    $('.mobile-tab').hide();
+    $('#burg').on('click', function() {
+      $('.mobile-tab').slideToggle(400);
+    });
+}
+
+function initCityModal() {
     $('.js-close-city-modal').on('click', closeCityModal);
     $('.js-next-city-modal').on('click', nextCityModal);
-})
 
-function closeCityModal() {
-    document.getElementById('city_dropdown').style.display = "none";
+    function closeCityModal() {
+        document.getElementById('city_dropdown').style.display = "none";
+    }
+    
+    function nextCityModal() {
+        document.getElementById('city_dropdown').style.display = "none";
+        document.getElementById('city_dropdown_2').style.display = "block";
+    }
 }
 
-function nextCityModal() {
-    document.getElementById('city_dropdown').style.display = "none";
-    document.getElementById('city_dropdown_2').style.display = "block";
+function initCollapse() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("footer-active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } 
+      });
+    }
 }
 
-/* Смена иконки сердце (избранное) при клике на него *для страницы Категорий, шаблон category.html */
+/* Смена иконки сердце (избранное) при клике на него *для страницы Категорий, шаблон category */
 function myFunction(x) {
     x.classList.toggle("fas");
 } 
-/* / Смена иконки сердце (избранное) при клике на него *для страницы Категорий, шаблон category.html */
+/* / Смена иконки сердце (избранное) при клике на него *для страницы Категорий, шаблон category */
 
 /* Смена иконки сердце (избранное) при клике на него *для страницы Избранные, шаблон favorite.html */
 function myFunction2(x) {
