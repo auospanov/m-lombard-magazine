dd9791bb986d9e0b96dcd2213fe5521f016aa323
Evgeniy Shek <shek@chocotravel.com>
2020-11-21 17:33:45 +0600

    chore(vue): add sourceMap for css

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  css: { sourceMap: true },
   chainWebpack: (config) => {
     config.resolve.alias.set('@shared', path.resolve(__dirname, './src/shared'));
     config.resolve.alias.set('@', path.resolve(__dirname, './src'));
