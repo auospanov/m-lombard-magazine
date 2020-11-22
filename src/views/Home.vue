041365289da93ff1a75af037395c49699a79c3c5
Evgeniy Shek <shek@chocotravel.com>
2020-11-22 15:20:24 +0600

    feature(home): add slider for mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <Slider />
-    <div class="home-goods">
+    <div class="home-goods home-goods--desktop">
       <div class="container">
         <div class="row goods-row">
           <div class="col-md-6 col-lg-6 col-sm-12 col-12">
@@ -73,16 +73,53 @@
         </div>
       </div>
     </div>
+    <div class="home-goods home-goods--mobile">
+      <div class="container">
+        <div class="col-12">
+          <vueper-slides
+            class="no-shadow"
+            :bullets="false"
+            :arrows="false"
+            :visible-slides="3"
+            slide-multiple
+            :dragging-distance="200"
+          >
+            <vueper-slide
+              v-for="(slide, i) in slides"
+              :key="i"
+              :image="slide.image"
+            />
+          </vueper-slides>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import { VueperSlides, VueperSlide } from 'vueperslides';
+import 'vueperslides/dist/vueperslides.css';
+
 import Slider from '@/components/home/Slider';
+
+function getSlidesForMobileGoods() {
+  return Array(12).fill({}).map((slide, index) => ({
+    image: require(`@/assets/images/img${index + 1}.png`),
+  }));
+}
 
 export default {
   name: 'Home',
   components: {
+    VueperSlides,
+    VueperSlide,
     Slider,
+  },
+  data() {
+    return {
+      slides: getSlidesForMobileGoods(),
+    };
   },
 };
 </script>
