28c0d7b5c756f5b41aa687a0ec1d8de2e69fdf37
Yessirkep <esik.95.kz@gmail.com>
2020-12-22 22:06:20 +0600

    add gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/m-lombard/': '/',
   css: { sourceMap: true },
   chainWebpack: (config) => {
     config.resolve.alias.set('@shared', path.resolve(__dirname, './src/shared'));
